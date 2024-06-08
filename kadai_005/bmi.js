825efef1508f23e4b82e965ba1f838c20e99ac1e
RYO06chan <nationaltintin@gmail.com>
2024-06-08 20:29:04 +0900

    kadai_005の追加

---
+++ kadai_005/bmi.js
@@ -0,0 +1,16 @@
+// 変数の宣言
+let height;
+let bodyWeight;
+
+// 値の代入
+height = 1.7
+bodyWeight = 68
+
+// 変数の宣言
+let bmi;
+
+// bmiの計算
+bmi = bodyWeight / (height * height);
+
+// コンソールへの出力
+console.log(bmi);
