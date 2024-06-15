682912bf0f33b8d25311a78590de7849d4fb6ec1
RYO06chan <nationaltintin@gmail.com>
2024-06-15 22:05:10 +0900

    kadai_mdn_jsの編集

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,5 +1,11 @@
 const today = new Date();
 
-console.log(today.getFullYear());
-console.log(today.getMonth() +1);
-console.log(today.getDate());
+const formatDate = () => {
+  const year = today.getFullYear();
+  const month = today.getMonth() + 1; // 0-11で表されるため、+1する
+  const day = today.getDate();
+
+  console.log(year + '年' + month + '月' + day + '日');
+};
+
+formatDate();
